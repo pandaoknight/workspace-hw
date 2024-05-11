b8c19c4736df8b7b891a9c7042490b852949914b
root <wkk>
2024-05-11 11:09:54 +0800

    feat: test resnet with simple k8s

--- scripts/mindspore_docker_build/docker_root_ascend-mindspore_A1/10_run.sh
+++ scripts/mindspore_docker_build/docker_root_ascend-mindspore_A1/10_run.sh
@@ -10,4 +10,5 @@ DEVICES="2"
 docker run -it --name ${CONTAINER_ID} \
 	-e ASCEND_VISIBLE_DEVICES=${DEVICES} \
 	-v /data/szl1160710/src/samples/:/home/HwHiAiUser/samples/ \
+	-v /data/szl1160710/scripts/mindspore_docker_build/docker_root_ascend-mindspore_A1/resnet50/:/home/HwHiAiUser/resnet50/ \
 	${IMAGE_TAG} /bin/bash
---
+++ scripts/mindspore_docker_build/docker_root_ascend-mindspore_A1/resnet50/clean.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+rm -rf BestCheckpoint  kernel_meta  kernel_meta_temp_*  rank_0
