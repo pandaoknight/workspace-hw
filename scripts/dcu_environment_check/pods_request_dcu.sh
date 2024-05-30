d64abbad3897dd29c941ad83604a966872ff89fc
root <higon>
2024-05-30 14:27:25 +0800

    feat: update pods check with dcu nums

--- scripts/dcu_environment_check/pods_request_dcu.sh
+++ scripts/dcu_environment_check/pods_request_dcu.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 {
-    echo "Namespace Name Status"
-    kubectl get pods --all-namespaces -o json | jq -r '.items[] | select(.spec.containers[].resources.requests."hygon.com/dcu" != null) | [.metadata.namespace, .metadata.name, .status.phase] | @tsv' | awk 'BEGIN {FS="\t"} {print $1, $2, $3}'
+    echo "Namespace Name Status DCU_Usage"
+    kubectl get pods --all-namespaces -o json | jq -r '.items[] | select(.spec.containers[].resources.requests."hygon.com/dcu" != null) | [.metadata.namespace, .metadata.name, .status.phase, (.spec.containers[].resources.requests."hygon.com/dcu")] | @tsv' | awk 'BEGIN {FS="\t"} {print $1, $2, $3, $4}'
 } | column -t
