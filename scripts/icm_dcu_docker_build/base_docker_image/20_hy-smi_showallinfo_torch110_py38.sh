200451823248f1bba7dd18bcc432cf5cc929401e
root <higon>
2024-06-05 16:37:26 +0800

    feat: hy-smi info for dtk2304 torch110 py38

---
+++ scripts/icm_dcu_docker_build/base_docker_image/20_hy-smi_showallinfo_torch110_py38.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+CONTAINER_ID="container-id-ap20"
+IMAGE="image.sourcefind.cn:5000/dcu/admin/base/pytorch:1.10.0-ubuntu20.04-dtk-23.04-py38-latest"
+
+docker run -it --network=host --name=${CONTAINER_ID} --privileged \
+  --device=/dev/kfd --device=/dev/dri --ipc=host --shm-size=32G -v /opt/hyhal:/opt/hyhal \
+  --group-add video --cap-add=SYS_PTRACE --security-opt seccomp=unconfined \
+  -u root \
+  --ulimit stack=-1:-1 --ulimit memlock=-1:-1 \
+  ${IMAGE} hy-smi -a
